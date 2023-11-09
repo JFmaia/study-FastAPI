99454d58d762c1779edf176a472879837dfa1c5d
JFmaia <jfmaia.dev@gmail.com>
2023-11-09 09:06:57 -0300

    Utilizando o method post

--- secao03/main.py
+++ secao03/main.py
@@ -1,8 +1,11 @@
+from typing import Optional
 from fastapi import FastAPI
 ## Para tratar excecão
 from fastapi import HTTPException
 ## Para ter todos os tipos de status
 from fastapi import status
+
+from models import Curso
 
 app = FastAPI()
 
@@ -31,7 +34,19 @@ async def get_curso(curso_id: int):
         return auxCurso
     except KeyError: ## KeyError é quando uma chave não é encontrada
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
+    
 
+## Mudando status da resposta de sucesso para 201
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+## Deixando o post ser optional mandar o curso
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
+
+
+## usado para executar a api com ```python main.py ```
 if __name__ == '__main__':
     import uvicorn
 
