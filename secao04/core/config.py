2f5b54bee593a88dd71aa31fe4c32effa4770aff
JFmaia <jfmaia.dev@gmail.com>
2023-11-14 22:00:40 -0300

    Criando primeiras tabelas git add .

--- secao04/core/config.py
+++ secao04/core/config.py
@@ -1,14 +1,14 @@
 """
 Pasta para configurações gerais usadas na aplicação
 """
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     
 
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "postgresql+async://jfmaia:p4ssw0rd@localhost:5432/faculdade"
+    DB_URL: str = "postgresql+asyncpg://postgres:pgdata741@localhost:5432/faculdade"
     DBBaseModel = declarative_base()
 
     class Config:
