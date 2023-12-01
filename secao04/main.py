be710fe271ec657363d3dbc3463cf39678fccd89
JFmaia <jfmaia.dev@gmail.com>
2023-12-01 16:09:51 -0300

    Modificando o main e ajustando o api.py

--- secao04/main.py
+++ secao04/main.py
@@ -1,21 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
+from api.v1.api import api_router
 
-app = FastAPI()
+app = FastAPI(title='Cursos API - FastAPI Alchemy')
 
-origins = [
-    "http://localhost:3000",
-]
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if __name__ == '__main__':
+    import uvicorn
 
-@app.get("/cursos")
-def root():
-    return {"message": "Welcome to FastAPI of Cursos"}
-    
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='info', reload=True)    
