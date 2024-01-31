528f656c4bcf03575c9cd376c0b4e48d6ea5fd89
JFmaia.dev <jfmaia.dev@gmail.com>
2024-01-31 10:49:10 -0300

    Ajustes necessarios para criar tabelas!

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -1,5 +1,4 @@
-from typing import List
-
+from typing import ClassVar
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,9 +9,13 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     API_V1_STR: str
 
-    DB_URL: str = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:{DATABASE_PORT}/{POSTGRES_DB}"
-    DBBaseModel = declarative_base()
+    @property
+    def DB_URL(self) -> str:
+        return f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@localhost:{self.DATABASE_PORT}/{self.POSTGRES_DB}'
+   
+    DBBaseModel: ClassVar = declarative_base()
 
+    #DIca de como gerar token no pront do python: token: str = secrets.token_urlsafe(32)
     JWT_SECRET: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
