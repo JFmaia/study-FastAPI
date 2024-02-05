0646d8fd2fd217d4e916f721b33cb6d82d53c83d
JFmaia.dev <jfmaia.dev@gmail.com>
2024-02-05 20:34:41 -0300

    Criando PUT do artigo!

--- secao06/api/v1/endpoints/artigo.py
+++ secao06/api/v1/endpoints/artigo.py
@@ -53,4 +53,28 @@ async def get_artigo(artigo_id: int, db: AsyncSession = Depends(get_session)):
         return artigo
     else:
         raise HTTPException(detail="Artigo não encontrado", status_code=status.HTTP_404_NOT_FOUND)
+
+#PUT Artigo
+@router.put('/{artigo_id}', response_model=ArtigoSchema, status_code=status.HTTP_202_ACCEPTED)
+async def get_artigo(artigo_id: int, artigo: ArtigoSchema, db: AsyncSession = Depends(get_session), usuario_logado: UsuarioModel = Depends(get_current_user)):
+    async with db as session:
+        query = select(ArtigoModel).filter(ArtigoModel.id == artigo_id)
+        result = await session.execute(query)
+        artigo_up = ArtigoModel = result.scalars().unique().one_or_none()
     
+        if artigo_up:
+            if artigo.titulo:
+                artigo_up.titulo = artigo.titulo
+            if artigo.descricao:
+                artigo_up.descricao = artigo.descricao
+            if artigo.url_fonte:
+                artigo_up.url_fonte = artigo.url_fonte
+            if usuario_logado.id != artigo_up.usuario_id:
+                artigo_up.usuario_id = usuario_logado.id
+
+            await session.commit()
+            
+            return artigo_up
+        
+        else:
+            raise HTTPException(detail="Artigo não encontrado", status_code=status.HTTP_404_NOT_FOUND)
