fca886182e2193c4de84e9acfab4c563940cb206
JFmaia.dev <jfmaia.dev@gmail.com>
2024-02-20 09:51:36 -0300

    Ajustando função de autenticação e criação de token!

--- secao06/core/auth.py
+++ secao06/core/auth.py
@@ -22,7 +22,7 @@ async def autenticar(email: EmailStr, senha:str, db: AsyncSession) -> Optional[U
     async with db as session:
         query = select(UsuarioModel).filter(UsuarioModel.email == email)
         result = await session.execute(query)
-        usuario: UsuarioModel = result.scalar().unique().one_or_none()
+        usuario: UsuarioModel = result.scalars().unique().one_or_none()
 
         if not usuario:
             return None
@@ -35,7 +35,7 @@ async def autenticar(email: EmailStr, senha:str, db: AsyncSession) -> Optional[U
 def _criar_token(tipo_token: str, tempo_vida: timedelta, sub:str) -> str:
     payload = {}
 
-    rn = timezone('America/Rio_Grande_Do_Norte')
+    rn = timezone('America/Fortaleza') 
     expira = datetime.now(tz=rn) + tempo_vida
 
     payload["type"] = tipo_token
